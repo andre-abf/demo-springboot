be53a0cece0e1a99c477b0855ebc1cf9e9fd6601
Andre-ctr <prof.andresos@gmail.com>
2025-11-23 20:06:42 -0300

    [FEATURE] Foi feito ajuste para o banco de dados e no Swagger

--- src/main/java/com/br/javainicio/demo_spring/DemoController.java
+++ src/main/java/com/br/javainicio/demo_spring/DemoController.java
@@ -1,5 +1,7 @@
 package com.br.javainicio.demo_spring;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +16,16 @@ public class DemoController {
         this.demoService = demoService;
     }
 
+    @Operation(summary = "Criando registro",
+            description = "Cria um novo registro Demo com base no ID e DESCRIPTION informado")
     @PostMapping
     public Demo createDemo(@RequestBody Demo demo){
         return demoService.saveDemo(demo);
     }
 
+    @Operation(summary = "Busca demo por ID", description = "Retorna um registro Demo com base no ID informado")
+    @ApiResponse(responseCode = "200", description = "Registro encontrado")
+    @ApiResponse(responseCode = "404", description = "Registro não encontrado")
     @GetMapping("/{id}")
     public Demo getDemo(@PathVariable("id") Long id){
         return demoService.getDemo(id).orElse(null);
