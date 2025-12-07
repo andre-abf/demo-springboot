f82d037228849855f2533e0b000ddeaf719a49c5
Andre-ctr <prof.andresos@gmail.com>
2025-12-07 10:02:29 -0300

    [FEATURE] Foi feita ajuste nos importes

--- src/main/java/com/br/javainicio/demo_spring/DemoServiceImpl.java
+++ src/main/java/com/br/javainicio/demo_spring/DemoServiceImpl.java
@@ -1,7 +1,6 @@
 package com.br.javainicio.demo_spring;
 
 import com.br.javainicio.demo_spring.entity.DemoEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
--- src/main/java/com/br/javainicio/demo_spring/DemoSpringApplication.java
+++ src/main/java/com/br/javainicio/demo_spring/DemoSpringApplication.java
@@ -1,19 +1,8 @@
 package com.br.javainicio.demo_spring;
 
 
-import java.util.Optional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 @SpringBootApplication
 public class DemoSpringApplication {
